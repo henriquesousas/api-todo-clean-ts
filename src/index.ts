ba38aa9e6fcc80654674d687104f7934962daf31
henriquesousas <paulohenriquess2014@gmail.com>
2023-04-17 14:12:01 -0300

    chore: include eslint

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from 'express';
 
-const app = express()
-const port = 5050
+const app = express();
+const port = 5050;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.json({message: "OK4"})
-})
+app.get('/', (_: Request, res: Response) => {
+  res.json({ message: 'OK' });
+});
 
 app.post('/user', (req: Request, res: Response) => {
-  res.status(201).json({message: req.body.name})
-})
+  res.status(201).json({ message: req.body.name });
+});
 
 app.listen(port, () => {
-  console.log(`App running in port ${port}`)
-})
+  console.log(`App running in port ${port}`);
+});
